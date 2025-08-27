60868c995f492de6a6c532ca6cae0069d0f5cb6c
João Vitor <jvsouzaj@gmail.com>
2025-08-27 09:16:49 -0300

    Uptade GeoJava
    Quase finalizando, projeto de geojava, trabalhando sobre areas planas

--- Geoplana.java
+++ Geoplana.java
@@ -1,4 +1,52 @@
-public class Geoplana {
-    
-    
+public class geoplana {
+ 
+    public static double areaCirculo(double raio) {
+
+        return Math.PI * raio * raio;
+
+    }
+ 
+    public static double perimetroCirculo(double raio) {
+
+        return 2 * Math.PI * raio;
+
+    }
+ 
+    public static double areaRetangulo(double base, double altura) {
+
+        return base * altura;
+
+    }
+ 
+    public static double perimetroRetangulo(double base, double altura) {
+
+        return 2 * (base + altura);
+
+    }
+ 
+    public static double areaTriangulo(double base, double altura) {
+
+        return (base * altura) / 2;
+
+    }
+ 
+    public static double perimetroTriangulo(double lado1, double lado2, double lado3) {
+
+        return lado1 + lado2 + lado3;
+
+    }
+ 
+    public static double areaPoligonoRegular(double lado, int n) {
+
+        return (n * lado * lado) / (4 * Math.tan(Math.PI / n));
+
+    }
+ 
+    public static double perimetroPoligonoRegular(double lado, int n) {
+
+        return n *lado;
+
+    }
+
 }
+ 
