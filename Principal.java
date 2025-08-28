5e74870ca284292d18db0d169e690ae2a0530507
Pedro r Leal <pedro08leal@gmail.com>
2025-08-28 11:58:38 -0300

    estamos corrigindo

--- Principal.java
+++ Principal.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
  
 public class principal {
 
+    private static Object geoplana;
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
